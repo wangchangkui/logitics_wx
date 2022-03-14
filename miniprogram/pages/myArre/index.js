4063c777513df86231691e71f4e591b40e2e6d61
Myk <17360402335@163.com>
2022-03-14 13:19:06 +0800

    更新一个页面的bug
    更新页面BUG

--- miniprogram/pages/myArre/index.js
+++ miniprogram/pages/myArre/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    orders:null
+    orders:null,
+    show:false
   },
 
   /**
@@ -32,9 +33,12 @@ Page({
       url: 'http://localhost:8080/user/arrea/'+user.userid,
       method:'GET',
       success:res=>{
-        that.setData({
-          orders:res.data.data
-        })
+        if(res.data.data.length>0){
+          that.setData({
+            orders:res.data.data,
+            show:false
+          })
+        }
       }
     })
   },
@@ -63,39 +67,4 @@ Page({
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
