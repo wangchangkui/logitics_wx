6319b1bbe31ca622ba1ea68e688a51cdf522ceae
Myk <17360402335@163.com>
2022-03-14 13:19:06 +0800

    更新页面BUG
    更新个人信息页面的bug

--- miniprogram/pages/chageInfo/index.js
+++ miniprogram/pages/chageInfo/index.js
@@ -109,7 +109,6 @@ Page({
           }
         })
       }).then(value => {
-      
         let users = {
           headerImage: value,
           username: that.data.name,
@@ -151,45 +150,10 @@ Page({
     }
   },
 
-
-
   onClickLeft: function () {
     wx.redirectTo({
       url: '../my/index',
     })
   },
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
